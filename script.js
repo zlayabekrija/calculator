4f1d5793ad3afc0df6958d4fc0a0e319d3e07ae2
zlajabekrija <zlaja.bekrija@gmail.com>
2018-10-27 23:21:52 +0200

    first logic

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-console.log(function calc(x){
-	return x;
-})
+let numbers = [];
+let operations = [];
+function calc(x) {
+	let value =x.value;
+	let preg=/(\D)/;
+	numbers.push(value);
+	let master=numbers.join('');
+	operations=master.split(preg);
+	
+	console.log(operations);
+	
+}
+
+function none() {
+
+	while (numbers.length > 0) {
+		numbers.pop();
+	}
+}
