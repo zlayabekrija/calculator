5525ddd11276975f15180c1978dc81322c198cd5
zlajabekrija <zlaja.bekrija@gmail.com>
2018-10-30 20:27:24 +0100

    changed logic added functions

--- script.js
+++ script.js
@@ -1,19 +1,57 @@
-let numbers = [];
-let operations = [];
-function calc(x) {
-	let value =x.value;
-	let preg=/(\D)/;
-	numbers.push(value);
-	let master=numbers.join('');
-	operations=master.split(preg);
-	
-	console.log(operations);
-	
+let getThem = [];
+
+function add(firstValue, secondValue) {
+	return firstValue + secondValue;
 }
 
-function none() {
+function subtract(firstValue, secondValue) {
+	return firstValue - secondValue;
+}
 
-	while (numbers.length > 0) {
-		numbers.pop();
+function multiply(firstValue, secondValue) {
+	return firstValue * secondValue;
+}
+
+function devide(firstValue, secondValue) {
+	return firstValue / secondValue;
+}
+
+function operate(firstValue, secondValue, operator) {
+	switch (operator) {
+		case '+':
+			return add(firstValue, secondValue);
+			break;
+		case '-':
+			return subtract(firstValue, secondValue);
+			break;
+		case '*':
+			return multiply(firstValue, secondValue);
+			break;
+		case '/':
+			return devide(firstValue, secondValue);
+			break;
+	}
+}
+
+function getValues(x) {
+	let firstValue = [];
+	let secondValue = [];
+	let operator = '';
+	let value = x.value;
+	let preg = /(\D)/;
+	let result = 0;
+	getThem.push(value);
+	let master = getThem.join('');
+	let aux = master.split(preg);
+	firstValue = parseInt(aux[0]);
+	secondValue = parseInt(aux[2]);
+	operator = aux[1];
+	console.log(firstValue, secondValue, operator);
+	if (firstValue && secondValue && operator) {
+		document.getElementById('result').innerHTML = firstValue + operator + secondValue;
+		result += operate(firstValue, secondValue, operator);
+		
+		console.log(result);
+		getThem = [];
 	}
 }
